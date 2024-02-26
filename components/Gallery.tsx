79bfeaa1fa8e0a427c0275ba28b976ebbc98c37b
Debam Roy <debamroy7@gmail.com>
2024-02-26 18:45:05 +0530

    grid

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -14,49 +14,49 @@ import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
 
 type GalleryProps = {
-    photos: Photo[];
-  };
+  photos: Photo[];
+};
 
 export function Gallery({ photos }: GalleryProps) {
-    const lightboxRef = useRef<LightGallery | null>(null);
-  
-    return (
-      <>
-        <Masonry breakpointCols={2} className="flex gap-2" columnClassName="">
-          {photos.map((photo, idx) => (
-            <div className="relative" key={photo.src}>
-              <Image
-                src={photo.src}
-                width={photo.width}
-                height={photo.height}
-                alt={photo.alt}
-                className="relative mb-2"
-                placeholder="blur"
-                blurDataURL={photo.blurDataURL}
-              />
-              <div
-                className="absolute w-full h-full inset-0 bg-transparent hover:bg-dark hover:bg-opacity-10 cursor-pointer"
-                onClick={() => {
-                  lightboxRef.current?.openGallery(idx);
-                }}
-              ></div>
-            </div>
-          ))}
-        </Masonry>
-        <LightGalleryComponent
-          onInit={(ref) => {
-            if (ref) {
-              lightboxRef.current = ref.instance;
-            }
-          }}
-          speed={500}
-          plugins={[lgThumbnail, lgZoom]}
-          dynamic
-          dynamicEl={photos.map((photo) => ({
-            src: photo.src,
-            thumb: photo.src,
-          }))}
-        />
-      </>
-    );
-  }
+  const lightboxRef = useRef<LightGallery | null>(null);
+
+  return (
+    <>
+      <Masonry breakpointCols={{default: 2, 800: 1}} className="flex gap-2" columnClassName="">
+        {photos.map((photo, idx) => (
+          <div className="relative" key={photo.src}>
+            <Image
+              src={photo.src}
+              width={photo.width}
+              height={photo.height}
+              alt={photo.alt}
+              className="relative mb-2"
+              placeholder="blur"
+              blurDataURL={photo.blurDataURL}
+            />
+            <div
+              className="absolute w-full h-full inset-0 bg-transparent hover:bg-dark hover:bg-opacity-10 cursor-pointer"
+              onClick={() => {
+                lightboxRef.current?.openGallery(idx);
+              }}
+            ></div>
+          </div>
+        ))}
+      </Masonry>
+      <LightGalleryComponent
+        onInit={(ref) => {
+          if (ref) {
+            lightboxRef.current = ref.instance;
+          }
+        }}
+        speed={500}
+        plugins={[lgThumbnail, lgZoom]}
+        dynamic
+        dynamicEl={photos.map((photo) => ({
+          src: photo.src,
+          thumb: photo.src,
+        }))}
+      />
+    </>
+  );
+}
